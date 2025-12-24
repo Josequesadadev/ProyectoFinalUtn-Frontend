c7813fb0eb69a09b6af228e98eb538479b6e54a9
Proyecto UTN <utn@example.com>
2025-12-24 19:32:27 -0300

    Frontend terminado + categorias

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,21 +1,13 @@
 const CATEGORIES = [
-  { id: 1, value: "tecnologia", content: "Tecnología" },
-  { id: 2, value: "electrohogar", content: "Electrohogar" },
-  { id: 3, value: "indumentaria", content: "Indumentaria" },
-  { id: 4, value: "calzado", content: "Calzado" },
-  { id: 5, value: "accesorios", content: "Accesorios" },
-  { id: 6, value: "deportes", content: "Deportes" },
-  { id: 7, value: "hogar", content: "Hogar" },
-  { id: 8, value: "muebles", content: "Muebles" },
-  { id: 9, value: "juguetes", content: "Juguetes" },
-  { id: 10, value: "belleza", content: "Belleza y Cuidado Personal" },
-  { id: 11, value: "salud", content: "Salud" },
-  { id: 12, value: "automotor", content: "Automotor" },
-  { id: 13, value: "mascotas", content: "Mascotas" },
-  { id: 14, value: "oficina", content: "Oficina y Papelería" },
-  { id: 15, value: "alimentos", content: "Alimentos y Bebidas" },
-  { id: 16, value: "herramientas", content: "Herramientas" },
-  { id: 17, value: "construccion", content: "Construcción" }
+  { id: 1, value: "gabinete", content: "gabinete" },
+  { id: 2, value: "fuente", content: "fuente" },
+  { id: 3, value: "placas", content: "placas" },
+  { id: 4, value: "monitor", content: "monitor" },
+  { id: 5, value: "auriculares", content: "auriculares" },
+  { id: 6, value: "mouse", content: "mouse" },
+  { id: 7, value: "teclado", content: "teclado" },
+  { id: 7, value: "microfono", content: "microfono" },
+
 ]
 
 export { CATEGORIES }
